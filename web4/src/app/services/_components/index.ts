e228fa0e154f544207bae336c9d147efb469423a
austin <austin@austindeMacBook-Air.local>
2025-09-09 21:36:12 +0800

    Clear all files from repository

--- web4/src/app/services/_components/index.ts
+++
@@ -1,4 +0,0 @@
-// Barrel file for services page components
-export { default as ServiceCard } from './ServiceCard';
-export { default as RelatedInsights } from './RelatedInsights';
-export * from './ServiceIcons';
